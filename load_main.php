3fa99d5cebba77b6372d98e259ce32c8c836b5c9
f0re7tpunk <f0re7tpunk@gmail.com>
2022-04-05 11:03:35 -0400

    dev1

--- load_main.php
+++ load_main.php
@@ -4,13 +4,14 @@ include("connection.php");
 $res = mysqli_query($connection,"SELECT * FROM $tbl_name WHERE type='item'");
     
     
-if (mysqli_num_rows($res) > 0){
+if (mysqli_num_rows($res) > 1){
     while ($row = mysqli_fetch_assoc($res)){
 
 
         $output .= "
         
         <div class='col-md-3 shadow-sm rounded bg-white d-flex justify-content-center'>
+            <a href=''>
             <div>
                 <img src='Assets/Product Images/".$row['model_number'].".png' href='details.php?id=".$row['model_number']."' class='col-md-12' height='200px'>
                 <h3 class='mx-3 text-center'>".$row['large_title']."</h3>
